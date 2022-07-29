d1aee4b23ce0b8262a1bc2fd8258db8809d6a80b
Raumo0 <dharapko@gmail.com>
2022-07-30 02:57:43 +0900

    Added Material UI lib

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,10 +7,7 @@ export default function Home() {
         <Layout>
             <Head>
                 <title>Blazarbit</title>
-                <link rel="icon" href="/favicon.ico" />
-                <link href='https://fonts.googleapis.com/css?family=Inter' rel='stylesheet'/>
             </Head>
-            body
         </Layout>
     )
 }
